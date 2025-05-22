69e5b492c5e2d764a90bcd26098a0661c08b8a92
Drilon Halili <drilon.halili@gmail.com>
2025-05-22 17:53:15 +0200

    Refactor expense validation and integrate @hono/zod-validator for schema validation

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,16 +1,16 @@
 import { Hono } from "hono"
+import { zValidator } from "@hono/zod-validator"
 import { z } from "zod"
 
-type Expense = {
-  id: number
-  title: string
-  amount: number
-}
-
-const createPostSchema = z.object({
-  title: z.string().min(1),
-  amount: z.number().min(0),
+const expenseSchema = z.object({
+  id: z.number().int().positive().min(1),
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive()
 })
+
+type Expense = z.infer<typeof expenseSchema>
+
+const createPostSchema = expenseSchema.omit({ id: true })
 
 const fakeExpenses: Expense[] = [
   { id: 1, title: "Groceries", amount: 100 },
@@ -19,14 +19,31 @@ const fakeExpenses: Expense[] = [
 ]
 
 export const expensesRoute = new Hono()
-  .get("/", (c) => {
-    return c.json({
-      expenses: fakeExpenses,
-    })
+  .get("/", async c => {
+    return c.json({ expenses: fakeExpenses })
   })
-  .post("/", async (c) => {
-    const data = await c.req.json()
-    const expense = createPostSchema.parse(data)
-  },)
-  // .delete
+  .post("/", zValidator("json", createPostSchema), async c => {
+    const expense = await c.req.valid("json")
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 })
+    c.status(201)
+    return c.json(expense)
+  })
+  .get("/:id{[0-9]+}", async c => {
+    const id = Number.parseInt(c.req.param("id"))
+    const expense = fakeExpenses.findIndex(expense => expense.id === id)
+    if (!expense) {
+      return c.notFound("Expense not found")
+    }
+    return c.json(expense)
+  })
+  .delete("/:id{[0-9]+}", async c => {
+    const id = Number.parseInt(c.req.param("id"))
+    const index = fakeExpenses.findIndex(expense => expense.id === id)
+    if (index === -1) {
+      return c.notFound("Expense not found")
+    }
+
+    const deletedExpense = fakeExpenses.splice(index, 1)[0]
+    return c.json({ expense: deletedExpense })
+  })
   // .put
